5ca9f2a051ae72219e4f7188e76b62ea9aa1a0b1
Sid Gopalan <36648847+SidG13@users.noreply.github.com>
2018-05-16 17:25:41 -0400

    Update Weasel.py

--- Weasel.py
+++ Weasel.py
@@ -1,12 +1,14 @@
 import random
 
-mutrate = 0.05
+childNumber = 100
+mut_rate = 0.05
 ref_string = "METHINKS*IT*IS*LIKE*A*WEASEL"
 
+
 # make the first random string
-histring=''
+histring = ''
 for i in range(len(ref_string)):
-    char = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "*")
+    char = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ*")
     histring += char
 print(histring)
 print("---------------")
@@ -16,29 +18,27 @@ print("---------------")
 def fitness_score(string2):
     return sum(100*((a == b)/(len(ref_string))) for a, b in zip(string2, ref_string))
 
-# mutate any string 100x, RETURN histring and hiscore
-def str_mutator(string):
-    childNumber=100
-    hiscore=0
-    for i in range(1, childNumber):
-        copy = list(string)
 
+# mutate any string a set number of times. Record the highest scoring string and its score
+def str_mutator(string):
+    hiscore = 0
+    for j in range(1, childNumber):
+        copy = list(string)
         for c in range(len(copy)):
-            char = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "*")
-            if random.random() < mutrate:
+            char = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ*")
+            if random.random() < mut_rate:
                 copy[c] = char
             string3 = ''.join(copy)
         if fitness_score(string3) > hiscore:
             hiscore = fitness_score(string3)
             histring = string3
-        #print(string3, fitness_score(string3))
     print("---------------")
     print(histring, hiscore)
     return histring
+
 
 counter = 0
 while histring != ref_string:
     histring = str_mutator(histring)
     counter += 1
 print("Number of generations was " + str(counter))
-
